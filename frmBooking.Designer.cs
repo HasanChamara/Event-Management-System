ebaeb3040099155cc2409b80ad88447737f95721
HasanChamara <hasanchamara1234@gmail.com>
2024-09-23 09:13:01 +0530

    View participants according to events

--- frmBooking.Designer.cs
+++ frmBooking.Designer.cs
@@ -28,40 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboEvents = new System.Windows.Forms.ComboBox();
+            this.dataGridViewEventParticipant = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.location = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEventParticipant)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(300, 25);
+            this.label2.Location = new System.Drawing.Point(317, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 31);
             this.label2.TabIndex = 6;
             this.label2.Text = "Bookings";
             // 
-            // dataGridView1
+            // label1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(50, 84);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(658, 150);
-            this.dataGridView1.TabIndex = 7;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Select Event";
+            // 
+            // comboEvents
+            // 
+            this.comboEvents.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboEvents.FormattingEnabled = true;
+            this.comboEvents.Location = new System.Drawing.Point(259, 83);
+            this.comboEvents.Name = "comboEvents";
+            this.comboEvents.Size = new System.Drawing.Size(279, 24);
+            this.comboEvents.TabIndex = 8;
+            this.comboEvents.SelectedIndexChanged += new System.EventHandler(this.comboEvents_SelectedIndexChanged);
+            // 
+            // dataGridViewEventParticipant
+            // 
+            this.dataGridViewEventParticipant.AllowUserToAddRows = false;
+            this.dataGridViewEventParticipant.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewEventParticipant.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewEventParticipant.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEventParticipant.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.location,
+            this.date,
+            this.description});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.LawnGreen;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewEventParticipant.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewEventParticipant.Location = new System.Drawing.Point(15, 130);
+            this.dataGridViewEventParticipant.Name = "dataGridViewEventParticipant";
+            this.dataGridViewEventParticipant.RowHeadersVisible = false;
+            this.dataGridViewEventParticipant.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewEventParticipant.Size = new System.Drawing.Size(748, 374);
+            this.dataGridViewEventParticipant.TabIndex = 10;
+            // 
+            // id
+            // 
+            this.id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.Width = 43;
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.name.HeaderText = "NAME";
+            this.name.Name = "name";
+            // 
+            // location
+            // 
+            this.location.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.location.HeaderText = "EMAIL";
+            this.location.Name = "location";
+            this.location.Width = 64;
+            // 
+            // date
+            // 
+            this.date.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.date.HeaderText = "PHONE";
+            this.date.Name = "date";
+            this.date.Width = 70;
+            // 
+            // description
+            // 
+            this.description.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.description.HeaderText = "ADDRESS";
+            this.description.Name = "description";
             // 
             // frmBooking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(775, 526);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridViewEventParticipant);
+            this.Controls.Add(this.comboEvents);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmBooking";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmBooking";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmBooking_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEventParticipant)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +155,13 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboEvents;
+        private System.Windows.Forms.DataGridView dataGridViewEventParticipant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn location;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn description;
     }
 }
--- frmBooking.cs
+++ frmBooking.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace EventManagementSystem
 {
@@ -15,6 +16,71 @@ namespace EventManagementSystem
         public frmBooking()
         {
             InitializeComponent();
+        }
+
+        string connectionString = "server=localhost;user=root;database=eventmanagement;port=3306;password=Chamara.19566";
+        private void frmBooking_Load(object sender, EventArgs e)
+        {
+            // retrieve event names to the combobox using a while loop and a datareader object
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM event";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    comboEvents.Items.Add(reader["event_name"].ToString());
+                }
+
+                connection.Close();
+            }
+        }
+
+        private void comboEvents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // get the selected event.
+            comboEvents.SelectedItem.ToString();
+
+            // get the event id of the selected event.
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT id FROM event WHERE event_name = '" + comboEvents.SelectedItem.ToString() + "'";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    frmBook.eventID = Convert.ToInt32(reader["id"]);
+                }
+
+                connection.Close();
+            }
+
+            // get the event participants of the selected event
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM participant WHERE id IN (SELECT participant_id FROM event_participant WHERE event_id = " + frmBook.eventID + ")";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                //clear data grid view
+                dataGridViewEventParticipant.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    dataGridViewEventParticipant.Rows.Add(reader["id"].ToString(), reader["name"].ToString(), reader["email"].ToString(), reader["phone"].ToString(), reader["address"].ToString());
+                }
+
+                connection.Close();
+            }
+             
         }
     }
 }
