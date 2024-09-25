1b480a6ca343b2bacf230c9891a7e8bc333438bb
HasanChamara <hasanchamara1234@gmail.com>
2024-09-25 21:35:38 +0530

    Participant Dashboard

---
+++ frmParticipantDashboard.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace EventManagementSystem
+{
+    partial class frmParticipantDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlSide = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnAddEvent = new System.Windows.Forms.Button();
+            this.pnlMain.SuspendLayout();
+            this.pnlSide.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.White;
+            this.pnlMain.Controls.Add(this.label1);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(220, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(823, 643);
+            this.pnlMain.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(182, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Event Management System";
+            // 
+            // pnlSide
+            // 
+            this.pnlSide.BackColor = System.Drawing.Color.PowderBlue;
+            this.pnlSide.Controls.Add(this.btnLogout);
+            this.pnlSide.Controls.Add(this.btnAddEvent);
+            this.pnlSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSide.Location = new System.Drawing.Point(0, 0);
+            this.pnlSide.Name = "pnlSide";
+            this.pnlSide.Size = new System.Drawing.Size(220, 643);
+            this.pnlSide.TabIndex = 2;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new System.Drawing.Point(0, 574);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(220, 40);
+            this.btnLogout.TabIndex = 5;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // btnAddEvent
+            // 
+            this.btnAddEvent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddEvent.Location = new System.Drawing.Point(0, 99);
+            this.btnAddEvent.Name = "btnAddEvent";
+            this.btnAddEvent.Size = new System.Drawing.Size(220, 40);
+            this.btnAddEvent.TabIndex = 1;
+            this.btnAddEvent.Text = "Event Management";
+            this.btnAddEvent.UseVisualStyleBackColor = true;
+            // 
+            // frmParticipantDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 643);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlSide);
+            this.Name = "frmParticipantDashboard";
+            this.Text = "frmParticipantDashboard";
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.pnlSide.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlSide;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnAddEvent;
+    }
+}
